aab9be5846a040ee523898d0e3c035fc58208535
TylenbM19 <e-e-dsf-dsf@yandex.ru>
2022-12-05 17:02:55 +0300

    ls

--- Test/main.cpp
+++ Test/main.cpp
@@ -6,8 +6,8 @@ int main(int argc, char* argv[])
     Parametrs parametrRocket;
     parametrRocket.DirectionX = 0.f;
     parametrRocket.DirectionY = 0.f;
-    parametrRocket.DirectionX = 0.f;
-    parametrRocket.DirectionX = 0.f;
+    parametrRocket.CoordinatsX = 0.f;
+    parametrRocket.CoordinatsX = 0.f;
     parametrRocket.Speed = 3.f;
 
     Parametrs parametrsTarget;
@@ -17,7 +17,7 @@ int main(int argc, char* argv[])
     parametrsTarget.CoordinatsY = 5.f;
     parametrsTarget.Speed = 1.f;
 
-    float DeltaTime = 0.f;
+    float DeltaTime = 0.1f;
 
     Imitation imitation(parametrRocket,parametrsTarget,DeltaTime);
     imitation.Update();
